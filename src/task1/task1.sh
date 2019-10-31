4acc760e674ca4b683f4f35e5b0e57850a792f06
Spartak Fomenko <spartakfomenko28@gmail.com>
2019-10-31 16:14:31 +0600

    add bash

---
+++ src/task1/task1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ~
+mkdir project5
+cd project5
+mkdir myproject
+cd myproject
+mkdir directory1 directory2 directory3
+cd directory1
+mkdir subdirectory1
+cd ..
+cd directory3
+mkdir subdirectory2
